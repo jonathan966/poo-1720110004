8303f4b8a7e8369ebde53ff9ccbc4034cc02a55b
JONATHAN GRANILLO <1720110004@utectulancingo.edu.mx>
2021-02-04 19:45:46 +0000

    semana3

--- semana3/distancia.py
+++ semana3/distancia.py
@@ -1,13 +1,13 @@
-from math import sqrt
+import math
 
-class Calculadora():
+class Puntos():
 
     def _init_(self):
         pass
 
     def distancia(self,x_1,y_1,x_2,y_2):
-        resultado = sqrt((x_2-x_1)**2+(y_2-y_1)**2)
+        resultado = math.sqrt((x_2-x_1)**2+(y_2-y_1)**2)
         print(round(resultado,2))
 
-objeto=Calculadora()
-objeto.distancia(7.88,3.17,4.78,4.99)
+objeto_puntos = Puntos()
+objeto_puntos.distancia(7.88,3.17,4.78,4.99)
